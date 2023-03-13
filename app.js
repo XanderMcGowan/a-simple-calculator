142003089a91a8c3eb70460aa9050e9903bbffcd
4621553 <xander.mcgowan@gmail.com>
2023-03-12 22:37:32 -0500

    created basic function adding counter funct:not completed

---
+++ app.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll('button')
+const screenDisplay = document.querySelector('.screen')
+
+let calculation = []
+let accumulativeCalculation
+let counterElm = 0
+let clicks = 0;
+
+function calculate (button) {
+  const value = button.textContent
+  if (value == "CLEAR") {
+    calculation = []
+    screenDisplay.textContent = '.'
+    document.getElementById("clicks").innerHTML = null;
+    clicks = null
+  } else if (value === "=") {
+    screenDisplay.textContent = eval(accumulativeCalculation) 
+  } else {
+    calculation.push(value)
+    accumulativeCalculation = calculation.join('')
+    screenDisplay.textContent = accumulativeCalculation
+  }
+
+onClick () 
+
+}
+
+
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  if (clicks >= 11) {
+    screenDisplay.textContent = 'ERROR'
+  }
+};
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button),))
+
